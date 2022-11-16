81d87aaa2df12240441b4d5ff1f238c515f972d9
TheCoolGuy <108559037+itsGamerOp@users.noreply.github.com>
2022-11-16 22:22:09 +0500

    Add files via upload

---
+++ spammer.py
@@ -0,0 +1,11 @@
+import pyautogui
+import time
+
+print("Made by a cool guy")
+print("Started")
+time.sleep(5)
+
+for i in range(100):
+    pyautogui.typewrite("jag jao bhai aj sab mar gye")
+    pyautogui.press("enter")
+    time.sleep(0.4)
